61e0570d55aab0da0284c30c2fd79d3fa0c992ae
ebraimsambo <ebraimsambo@gmail.com>
2024-10-08 05:10:38 +0100

    gise

--- src/database/types/schema.d.ts
+++ src/database/types/schema.d.ts
@@ -1,4 +1,4 @@
-import * as schema from 'src/database/schemas/user.schema';
+import * as schema from 'src/database/schemas';
 import { NodePgDatabase } from 'drizzle-orm/node-postgres';
 
 export type DrizzleDB = NodePgDatabase<typeof schema>;
